20e8f0a30220aaf33497abf77ab378f4a813fc1c
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-09-24 08:29:08 +0900

    prototype: add playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests/node',
+  reporter: [['list']],
+  use: { trace: 'on-first-retry', screenshot: 'only-on-failure', video: 'retain-on-failure' },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+    { name: 'mobile-chromium', use: { ...devices['Pixel 7'] } },
+    { name: 'mobile-webkit', use: { ...devices['iPhone 13'] } },
+  ],
+});
