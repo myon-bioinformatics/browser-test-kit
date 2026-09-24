1efe2de43a49c393fcb64c8dc20251804e05902e
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-09-24 09:10:14 +0900

    fix: address review for playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests/node',
   reporter: [['list']],
+  retries: process.env.CI ? 1 : 0,
   use: { trace: 'on-first-retry', screenshot: 'only-on-failure', video: 'retain-on-failure' },
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
